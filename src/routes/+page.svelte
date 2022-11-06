273fe0bd87f5be909473cd2369467c7b23e78775
Andris Shvarcs <andris.svarcs@yahoo.com>
2022-11-06 11:55:17 +0000

    Basic design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,77 @@
-
 <svelte:head>
-  <title>Robins Svarcs - cinematographer</title>
-  <meta name="description"
-        content="Amateur cinematographer and video editor" />
+	<title>Robins Svarcs - cinematographer</title>
+	<meta name='description'
+				content='Amateur cinematographer and video editor' />
 </svelte:head>
 
-<div class="grid">
-  <section>
-    <h1>Robins Svarcs</h1>
-    <h2>cinematographer</h2>
-    <p>Amateur cinematographer and video editor</p>
-  </section>
+<div class="wrap">
+	<section class="grid grid-title">
+		<div>
+			<h1>Robins Svarcs</h1>
+		</div>
+		<div>
+			<h2>cinematographer &amp; video editor</h2>
+		</div>
+	</section>
+
+	<section class="grid">
+		<div>
+			<p>about</p>
+			<p>experience</p>
+			<p>works</p>
+			<br>
+			<p>LinkedIn</p>
+			<p>LinkedIn</p>
+			<p>LinkedIn</p>
+			<p>LinkedIn</p>
+		</div>
+		<div>
+			<p>Portfolio video</p>
+			<div>VIDEO HERE</div>
+		</div>
+	</section>
 </div>
 
 <style>
+		.wrap {
+				padding: 0 8px;
+		}
+
     .grid {
         display: grid;
-        grid-template-columns: 1fr;
+        grid-template-columns: 124px 1fr;
         margin: 0 auto;
-        max-width: 500px;
-        padding-bottom: 80px;
+				gap: 8px;
+				max-width: 800px;
+
+        background-color: yellow;
     }
 
-    section {
-        padding: 0 1rem;
+		.grid-title {
+				min-height: 64px;
+				align-items: center;
     }
 
-    @media (min-width: 800px) {
+    .grid > div {
+        outline: 1px solid red;
+    }
+
+    h1 {
+        font-size: 2rem;
+        margin: 0;
+    }
+
+		h2 {
+				font-size: 1.2rem;
+        text-align: center;
+    }
+
+    /*@media (min-width: 800px) {
         .grid {
-            grid-template-columns: 1fr 1fr;
+            grid-template-columns: 260px 1fr;
             max-width: 1200px;
         }
-    }
+    }*/
 
     @media (min-width: 900px) {
         section {
