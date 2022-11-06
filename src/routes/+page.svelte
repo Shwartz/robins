a48cfbfca1f51fc1e0b45c2bf449adeec71fddea
Andris Shvarcs <andris.svarcs@yahoo.com>
2022-11-06 12:28:32 +0000

    Adding some design aligniment and video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,9 @@
 				content='Amateur cinematographer and video editor' />
 </svelte:head>
 
-<div class="wrap">
-	<section class="grid grid-title">
-		<div>
+<div class='wrap'>
+	<section class='grid grid-title'>
+		<div class='logo'>
 			<h1>Robins Svarcs</h1>
 		</div>
 		<div>
@@ -14,8 +14,8 @@
 		</div>
 	</section>
 
-	<section class="grid">
-		<div>
+	<section class='grid grid-content'>
+		<div class='nav'>
 			<p>about</p>
 			<p>experience</p>
 			<p>works</p>
@@ -25,57 +25,76 @@
 			<p>LinkedIn</p>
 			<p>LinkedIn</p>
 		</div>
-		<div>
-			<p>Portfolio video</p>
-			<div>VIDEO HERE</div>
+		<div class='center'>
+			<h3>Portfolio video</h3>
+			<div>
+				<iframe width='100%' height='315' src='https://www.youtube.com/embed/p6zWyxNrHO0' title='YouTube video player'
+								frameborder='0'
+								allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+								allowfullscreen></iframe>
+			</div>
 		</div>
 	</section>
 </div>
 
-<style>
-		.wrap {
-				padding: 0 8px;
-		}
+<style lang='scss'>
+  .wrap {
+    padding: 0 8px;
+  }
 
-    .grid {
-        display: grid;
-        grid-template-columns: 124px 1fr;
-        margin: 0 auto;
-				gap: 8px;
-				max-width: 800px;
+  .grid {
+    display: grid;
+    grid-template-columns: 124px 1fr;
+    margin: 0 auto;
+    gap: 8px;
+    max-width: 800px;
+  }
 
-        background-color: yellow;
-    }
+  .grid-title {
+    min-height: 96px;
+    align-items: center;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.25);
 
-		.grid-title {
-				min-height: 64px;
-				align-items: center;
-    }
-
-    .grid > div {
-        outline: 1px solid red;
-    }
+    /*background-color: rgba(234, 152, 152, 0.25);*/
 
     h1 {
-        font-size: 2rem;
-        margin: 0;
+      height: 64px;
+      align-self: center;
     }
+  }
 
-		h2 {
-				font-size: 1.2rem;
-        text-align: center;
+  .grid-content {
+    min-height: calc(100vh - 96px);
+
+    .nav {
+      border-right: 1px solid rgba(0, 0, 0, 0.25);
+      padding-right: 8px;
     }
+  }
 
-    /*@media (min-width: 800px) {
-        .grid {
-            grid-template-columns: 260px 1fr;
-            max-width: 1200px;
-        }
-    }*/
+  .grid > div {
+    /*outline: 1px solid rgba(234, 152, 152, 0.25);*/
+  }
 
-    @media (min-width: 900px) {
-        section {
-            padding: 0 2rem;
-        }
+  .logo {
+    display: flex;
+    height: 100%;
+    border-right: 1px solid rgba(0, 0, 0, 0.25);
+  }
+
+  h1 {
+    font-size: 2rem;
+    margin: 0;
+  }
+
+  h2 {
+    font-size: 1.2rem;
+    text-align: center;
+  }
+
+  /*@media (min-width: 900px) {
+    section {
+      padding: 0 2rem;
     }
+  }*/
 </style>
