972b4f357070a6cf13463c2895db771d861dde64
Andris Shvarcs <andris.svarcs@yahoo.com>
2023-02-01 13:21:53 +0000

    Adding small images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,28 @@
 <script>
 	import { lazyLoad } from '$lib/utils/lazyLoad.js';
-	import img1 from '$lib/images/0ABAE99A.jpg';
-	import img2 from '$lib/images/5119AAC1.jpg';
-	import img3 from '$lib/images/64425FB0.jpg';
-	import img4 from '$lib/images/673268FE.jpg';
-	import img5 from '$lib/images/003215340002.jpg';
-	import img6 from '$lib/images/003215340017.jpg';
+	import img1 from '$lib/images/art-01.jpg';
+	import img2 from '$lib/images/art-02.jpg';
+	import img3 from '$lib/images/art-03.jpg';
+	import img4 from '$lib/images/art-04.jpg';
+	import img5 from '$lib/images/art-05.jpg';
+	import img6 from '$lib/images/art-06.jpg';
+	import img7 from '$lib/images/art-07.jpg';
+	import img8 from '$lib/images/art-08.jpg';
+	import img9 from '$lib/images/art-09.jpg';
+	import img10 from '$lib/images/art-10.jpg';
+	import img11 from '$lib/images/art-11.jpg';
+	import img12 from '$lib/images/art-12.jpg';
+	import img13 from '$lib/images/art-13.jpg';
+	import img14 from '$lib/images/art-14.jpg';
+	import img15 from '$lib/images/art-15.jpg';
+	import img16 from '$lib/images/art-16.jpg';
+	import img17 from '$lib/images/art-17.jpg';
+	import img18 from '$lib/images/art-18.jpg';
+	import img19 from '$lib/images/art-19.jpg';
+	import img20 from '$lib/images/art-20.jpg';
+	import img21 from '$lib/images/art-21.jpg';
+	import robinsIcon from '$lib/images/robins-icon.jpg';
+
 	export let images = [
 		{ src: img1, alt: 'Image 1' },
 		{ src: img2, alt: 'Image 2' },
@@ -13,6 +30,21 @@
 		{ src: img4, alt: 'Image 4' },
 		{ src: img5, alt: 'Image 5' },
 		{ src: img6, alt: 'Image 6' },
+		{ src: img7, alt: 'Image 7' },
+		{ src: img8, alt: 'Image 8' },
+		{ src: img9, alt: 'Image 9' },
+		{ src: img10, alt: 'Image 10' },
+		{ src: img11, alt: 'Image 11' },
+		{ src: img12, alt: 'Image 12' },
+		{ src: img13, alt: 'Image 13' },
+		{ src: img14, alt: 'Image 14' },
+		{ src: img15, alt: 'Image 15' },
+		{ src: img16, alt: 'Image 16' },
+		{ src: img17, alt: 'Image 17' },
+		{ src: img18, alt: 'Image 18' },
+		{ src: img19, alt: 'Image 19' },
+		{ src: img20, alt: 'Image 20' },
+		{ src: img21, alt: 'Image 21' }
 	];
 
 </script>
@@ -25,15 +57,17 @@
 
 <div class='wrap'>
 	<header>
-		<h2>Robins Svarcs</h2>
+		<h2 class='intro-title'>
+			<img src={robinsIcon} alt='Robins'/>
+			<span>Robins Svarcs</span>
+		</h2>
 		<h1>Cinematographer</h1>
 		<p>Amateur cinematographer and video editor studying at Baltic Film & Media School in Tallinn, Estonia. Throughout
 			the years, has worked on several music videos, advertisements, movie sets and events.</p>
 		<p>
 			<a href='https://www.instagram.com/cetracis' target='_blank' rel='noopener noreferrer'>Instagram</a> | <a
-				href='https://www.linkedin.com/in/cetracis/' target='_blank' rel='noopener noreferrer'>LinkedIn</a>
+			href='https://www.linkedin.com/in/cetracis/' target='_blank' rel='noopener noreferrer'>LinkedIn</a>
 		</p>
-
 	</header>
 
 	<section>
@@ -61,61 +95,96 @@
 	</section>
 
 	<footer>
-		<p>© 2021 Robins Svarcs</p>
+		<p>© 2023 Robins Svarcs</p>
 	</footer>
 </div>
 
 <style lang='scss'>
- header {
-	 max-width: 800px;
-	 text-align: center;
-	 margin: 5em auto 0;
- }
- h1 {
-	 margin: 2rem 0 4rem;
-   font-family: 'Lobster', cursive;
-	 font-size: 6em;
-   color: #f25e22;
- }
- h2 {
-	 font-size: 1.7em;
- }
- p {
-	 font-size: 1.5em;
-	 line-height: 1.5;
- }
+  header {
+    max-width: 800px;
+    text-align: center;
+    margin: 5em auto 0;
+  }
 
- /** content */
- section {
-   margin: 6rem 0 0;
-   padding: 4rem;
+  h1 {
+    margin: 2rem 0 4rem;
+    font-family: 'Lobster', cursive;
+    /*font-size: 6em;*/
+		font-size: 12vw;
+    line-height: 12vw;
+    color: #f25e22;
 
-   h2 {
-     text-align: center;
-     margin: 2rem 0 4rem;
-		 color: #f25e22;
-   }
- }
- .section-grey {
-   background: #f3f3f2;
- }
+    visibility: visible;
+    opacity: 1;
+     transform: translateY(0) scale(1);
+		-webkit-transform: translateY(0) scale(1);
+    -webkit-transition: -webkit-transform 0.45s ease 0.48s, opacity 0.45s ease 0.48s;
+     transition: transform 0.45s ease 0.48s, opacity 0.45s ease 0.48s;
+  }
 
- .grid-images {
-	 display: grid;
-	 grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-	 grid-gap: 1rem;
-   max-width: 1200px;
-	 margin: 0 auto;
+  @media (min-width: 768px) {
+    h1 {
+      font-size: 6rem;
+      line-height: 12vw;
+    }
+  }
 
-   img {
-     opacity: 0;
-     transition: all 3s ease;
-   }
- }
+  .intro-title {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+    font-size: 1.7em;
 
- footer {
-	 padding: 2rem 0;
-	 border-top: 1px solid #ccc;
-	 text-align: center;
- }
+		img {
+			width: 1.5em;
+			height: 1.5em;
+			margin-right: 0.5em;
+			border-radius: 50%;
+			box-shadow: 0 0 0.8em 1px rgba(242, 94, 34, 0.8);
+		}
+  }
+
+  p {
+    font-size: 1.5em;
+    line-height: 1.5;
+  }
+
+  /** content */
+  section {
+    margin: 6rem 0 0;
+    padding: 1rem;
+
+    h2 {
+      text-align: center;
+      margin: 2rem 0 3rem;
+      color: #f25e22;
+    }
+
+    @media (min-width: 768px) {
+      padding: 1rem;
+		}
+  }
+
+  .section-grey {
+    background: #f3f3f2;
+  }
+
+  .grid-images {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
+    grid-gap: 1rem;
+    max-width: 1200px;
+    margin: 0 auto;
+
+    img {
+      opacity: 0;
+      transition: all 2s ease;
+    }
+  }
+
+  footer {
+    padding: 2rem 0;
+    border-top: 1px solid #ccc;
+    text-align: center;
+  }
 </style>
